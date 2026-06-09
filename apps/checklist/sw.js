6e0b7a4825d47cf4a09013ca4f3ff56d533f1dfe
Matthieu-Vachet <79032023+Matthieu-Vachet@users.noreply.github.com>
2026-06-09 18:25:05 +0200

    Update service worker cache version to v7 for improved asset management

--- apps/checklist/sw.js
+++ apps/checklist/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pokedex-checklist-studio-v6";
+const CACHE = "pokedex-checklist-studio-v7";
 const SHELL = ["/", "/manifest.json", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
